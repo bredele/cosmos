137adee02e22c3a97bd5b3e107e08b2e10314d8c
bredele <olivier.wietrich@gmail.com>
2014-03-24 23:25:11 -0600

    try to get zip

--- utils/package.js
+++ utils/package.js
@@ -6,14 +6,31 @@
 var request = require('superagent');
 
 
+function parse(res, cb) {
+  res.setEncoding('binary');
+  res.data = '';
+  res.on('data', function (chunk) {
+    res.data += chunk;
+  });
+  res.on('end', function () {
+    cb(null, new Buffer(res.data, 'binary'));
+  });
+}
+
 /**
- * Expose 'Mod'
+ * Expose 'request'
  */
 
 module.exports.request = function(repo, name) {
-	//NOTE:we could set branch with @
-	var url = '/' + repo + '/' + name + '/archive/master.zip';
-	request.get('https://github.com' + url, function(res){
-		console.log(res);
-	});
+  //NOTE:we could set branch with @
+  var url = '/' + repo + '/' + name + '/zip/master';
+  request
+    .get('https://codeload.github.com' + url)
+    .parse(parse)
+    .end(function(res) {
+      //if (err) return err;
+      console.log(Buffer.isBuffer(res.body));
+      //Buffer.isBuffer(res.body)
+      console.log(res.body);
+    });
 };
