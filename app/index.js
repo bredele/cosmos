e9cd43482df7fbeebf7e6d840c99811f42c82763
bredele <olivier.wietrich@gmail.com>
2014-03-26 20:52:25 -0600

    add build static folder

--- app/index.js
+++ app/index.js
@@ -16,6 +16,7 @@ var app = module.exports = express();
 app.use(express.logger('dev'));
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 app.use(express.errorHandler());
 
 
