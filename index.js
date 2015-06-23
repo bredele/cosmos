bb13c18a56891341df626b75e8132f4c5f795008
bredele <olivier.wietrich@gmail.com>
2015-06-22 21:30:20 -0600

    reload cosmos app

--- index.js
+++ index.js
@@ -22,16 +22,70 @@ var rood = require('roof');
 
 module.exports = function(name) {
 
+	/**
+	 * Cosmos app path.
+	 * @type {String}
+	 */
+	
 	var path = process.cwd();
 
-	var html = read(join(path, name + '.html'), 'utf8');
 
-	var json = JSON.parse(read(join(path, name + '.json'), 'utf8'));
+	/**
+	 * A cosmos app is a brick.
+	 * 
+	 * @see  http://github.com/bredele/brickjs
+	 */
+	
+	var that = brick();
 
-	// @note may be use htmlclean to clean comments, line breaks
-	// etc (could be options)
-	var that = brick(html.replace(/(\r\n|\n|\r)/gm,""), json.data);
 
-	return that.render();
+	/**
+	 * Reload cosmos app.
+	 *
+	 * Load template and interpolate
+	 * it with data.
+	 * 
+	 * @return {this}
+	 * @api public
+	 */
+	
+	that.reload = function() {
+		that.off();
+		html();
+		json();
+		that.render();
+		return that;
+	};
+
+
+	/**
+	 * Read HTML template
+	 * and set cosmos template.
+	 *
+	 * @note should clean html 
+	 * as option (htmlclean).
+	 * 
+	 * @api private
+	 */
+	
+	function html() {
+		var str = read(join(path, name + '.html'), 'utf8');
+		that.from(str.replace(/(\r\n|\n|\r)/gm,""));
+	}
+
+
+	/**
+	 * Read cosmos configuration
+	 * and data.
+	 * 
+	 * @api private
+	 */
+	
+	function json() {
+		var obj = JSON.parse(read(join(path, name + '.json'), 'utf8'));
+		that.reset(obj.data);
+	}
+
+	return that;
 };
 
